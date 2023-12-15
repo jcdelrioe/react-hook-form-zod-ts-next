34279f4c06d55aaf7fcf8c9fb3a7cfd8e219c6a0
Juan Carlos Del Rio <jcdre@hotmail.com>
2023-12-15 18:29:57 -0500

    add zod, date of birth, validatios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,77 @@
 "use client"
 
-import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm, SubmitHandler } from "react-hook-form"
+import { userSchema, mappedPlans } from "./validations/userSchema"
+
+type Inputs = {
+  name: string
+  email: string
+  password: string
+  confirmPassword: string
+  dateOfBirth: string
+  weight: string
+  plan: string
+}
 
 export default function Home() {
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: zodResolver(userSchema),
+  })
+
+  const plansOptions = Object.entries(mappedPlans).map(([key, value]) => (
+    <option value={key} key={key}>
+      {value}
+    </option>
+  ))
+
+  console.log(errors)
+
+  const onsubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+  }
 
   return (
     <div>
-      <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <form onSubmit={handleSubmit(onsubmit)}>
         <label htmlFor="name">Name</label>
         <input type="text" id="name" {...register("name")} />
-
+        {errors.name?.message && <p>{errors.name?.message}</p>}
         <label htmlFor="email">Email</label>
         <input type="email" id="email" {...register("email")} />
-
+        {errors.email?.message && <p>{errors.email?.message}</p>}
         <label htmlFor="password">Password</label>
         <input type="password" id="password" {...register("password")} />
-
+        {errors.password?.message && <p>{errors.password?.message}</p>}
         <label htmlFor="confirmPassword">confirm Password</label>
         <input
           type="password"
           id="confirmPassword"
           {...register("confirmPassword")}
         />
+        {errors.confirmPassword?.message && (
+          <p>{errors.confirmPassword?.message}</p>
+        )}
 
+        <label htmlFor="dateOfBirth">Date of birth</label>
+        <input type="number" id="dateOfBirth" {...register("dateOfBirth")} />
+        {errors.dateOfBirth?.message && <p>{errors.dateOfBirth?.message}</p>}
         <label htmlFor="weight">Weight</label>
         <input type="number" id="weight" {...register("weight")} />
-
+        {errors.weight?.message && <p>{errors.weight?.message}</p>}
         <label htmlFor="plan">Plan</label>
         <select {...register("plan")} id="plan">
-          <option value="free">Free</option>
-          <option value="basic">Basic</option>
-          <option value="medium">Medium</option>
-          <option value="premium">Premium</option>
+          {plansOptions}
         </select>
+        {errors.plan?.message && <p>{errors.plan?.message}</p>}
         <button type="submit">Submit</button>
       </form>
+      <div>{JSON.stringify(watch(), null, 2)}</div>
     </div>
   )
 }
--- src/app/validations/userSchema.ts
+++ src/app/validations/userSchema.ts
@@ -1,29 +1,48 @@
-import { z } from "zod"
+import { date, z } from "zod"
 
 const plans = ["free", "basic", "medium", "premium"] as const
 
-export const userSchema = z.object({
-  name: z
-    .string()
-    .min(3, {
-      message: "Name must be at least 3 characters",
-    })
-    .max(200, {
-      message: "Name must be at most 200 characters",
+export type Plans = (typeof plans)[number]
+
+export const mappedPlans: { [key in Plans]: string } = {
+  basic: "Basic",
+  free: "Free",
+  medium: "Medium",
+  premium: "Premium",
+}
+
+export const userSchema = z
+  .object({
+    name: z
+      .string()
+      .min(3, {
+        message: "Name must be at least 3 characters",
+      })
+      .max(200, {
+        message: "Name must be at most 200 characters",
+      }),
+    email: z.string().email({
+      message: "Invalid email",
     }),
-  email: z.string().email({
-    message: "Invalid email",
-  }),
-  password: z.string().min(6, {
-    message: "Name must be at least 6 characters",
-  }),
-  confirmPassword: z.string().min(6, {
-    message: "Name must be at least 6 characters",
-  }),
-  weight: z.string().refine((weight) => !isNaN(parseFloat(weight)), {
-    message: "Weight must be a number",
-  }),
-  plan: z.enum(plans, {
-    errorMap: () => ({ message: "Invalid plan" }),
-  }),
-})
+    password: z.string().min(6, {
+      message: "Name must be at least 6 characters",
+    }),
+    confirmPassword: z.string().min(6, {
+      message: "Name must be at least 6 characters",
+    }),
+    dateOfBirth: z
+      .string()
+      .refine((dob) => new Date(dob).toString() !== "Invalid Date", {
+        message: "Invalid date",
+      }),
+    weight: z.string().refine((weight) => !isNaN(parseFloat(weight)), {
+      message: "Weight must be a number",
+    }),
+    plan: z.enum(plans, {
+      errorMap: () => ({ message: "Invalid plan" }),
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  })
